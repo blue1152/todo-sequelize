d462f787082689e161ff0121799e0c1057a8ea08
blue1152 <bluememory1152@gmail.com>
2019-12-20 18:07:41 +0800

    add routes and views for register and login pages

--- app.js
+++ app.js
@@ -14,6 +14,27 @@ app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+// 認證系統的路由
+// 登入頁面
+app.get("/users/login", (req, res) => {
+  res.render("login");
+});
+// 登入檢查
+app.post("/users/login", (req, res) => {
+  res.send("login");
+});
+// 註冊頁面
+app.get("/users/register", (req, res) => {
+  res.render("register");
+});
+// 註冊檢查
+app.post("/users/register", (req, res) => {
+  res.send("register");
+});
+// 登出
+app.get("/users/logout", (req, res) => {
+  res.send("logout");
+});
 
 // port
 app.listen(3000, () => {
