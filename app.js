871f7433844d8f2ac796d06da6137ca83bb4be81
blue1152 <bluememory1152@gmail.com>
2019-12-25 18:42:58 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const passport = require("passport");
 
 // database
 const db = require("./models");
@@ -14,7 +16,21 @@ app.set("view engine", "handlebars");
 // other middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
+app.use(
+  session({
+    secret: "your secret key",
+    resave: "false",
+    saveUninitialized: "false"
+  })
+);
+// 使用 Passport
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 // 使用路由器
 app.use("/", require("./routes/home"));
 app.use("/users", require("./routes/user"));
